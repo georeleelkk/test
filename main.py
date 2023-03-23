fe429866f01bf83a9df294929a39ab3d9c7a37b0
georeleelkk <128667336+georeleelkk@users.noreply.github.com>
2023-03-23 12:32:10 +0800

    ph meter

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-voltage = 0
+_temperature      = 25.0
+_acidVoltage      = 2032.44
+_neutralVoltage   = 1500.0
 
-def on_forever():
-    pass
-basic.forever(on_forever)
+voltat7 = 0
+vlotat4 = 0
 
-def on_every_interval():
-    global voltage
-    voltage = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
-    basic.show_number(voltage)
-loops.every_interval(5000, on_every_interval)
+def on_button_pressed_a():
+    global vlotat4
+    basic.show_string("Cal mode")
+    basic.show_string("Ph4")
+    vlotat4 = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global voltat7
+    basic.show_string("Cal mode")
+    basic.show_string("Ph7")
+    voltat7 = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def read_PH(voltage,temperature):
+    global _acidVoltage
+    global _neutralVoltage
+    slope     = (7.0-4.0)/((_neutralVoltage-1500.0)/3.0 - (_acidVoltage-1500.0)/3.0)
+    intercept = 7.0 - slope*(_neutralVoltage-1500.0)/3.0
+    _phValue  = slope*(voltage-1500.0)/3.0+intercept
+    
+    return Math.round(_phValue)
+
+def on_button_pressed_ab():
+    read_PH(pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000, input.temperature())
+input.on_button_pressed(Button.B, on_button_pressed_b)
