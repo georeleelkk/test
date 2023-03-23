f3a978eb9226c56803ad1befe1b6a9f3483c9b97
georeleelkk <128667336+georeleelkk@users.noreply.github.com>
2023-03-23 12:47:32 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-_temperature      = 25.0
-_acidVoltage      = 2032.44
-_neutralVoltage   = 1500.0
-
-voltat7 = 0
-vlotat4 = 0
+def on_button_pressed_ab():
+    read_PH(pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000,
+        input.temperature())
 
 def on_button_pressed_a():
-    global vlotat4
+    global _acidVoltage, _neutralVoltage
     basic.show_string("Cal mode")
     basic.show_string("Ph4")
-    vlotat4 = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
+    basic.show_number(3)
+    basic.pause(1000)
+    basic.show_number(2)
+    basic.pause(1000)
+    basic.show_number(1)
+    basic.pause(1000)
+    _acidVoltage = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
+    basic.show_string("Ph7")
+    basic.show_number(3)
+    basic.pause(1000)
+    basic.show_number(2)
+    basic.pause(1000)
+    basic.show_number(1)
+    basic.pause(1000)
+    _neutralVoltage = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    global voltat7
-    basic.show_string("Cal mode")
-    basic.show_string("Ph7")
-    voltat7 = pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def read_PH(voltage,temperature):
-    global _acidVoltage
-    global _neutralVoltage
-    slope     = (7.0-4.0)/((_neutralVoltage-1500.0)/3.0 - (_acidVoltage-1500.0)/3.0)
-    intercept = 7.0 - slope*(_neutralVoltage-1500.0)/3.0
-    _phValue  = slope*(voltage-1500.0)/3.0+intercept
-    
+def read_PH(voltage: number, temperature: number):
+    global slope, intercept, _phValue
+    slope = (7 - 4) / ((_neutralVoltage - 1500) / 3 - (_acidVoltage - 1500) / 3)
+    intercept = 7 - slope * (_neutralVoltage - 1500) / 3
+    _phValue = slope * (voltage - 1500) / 3 + intercept
     return Math.round(_phValue)
+_phValue = 0
+intercept = 0
+slope = 0
 
-def on_button_pressed_ab():
-    read_PH(pins.analog_read_pin(AnalogPin.P0) / 1024 * 5000, input.temperature())
-input.on_button_pressed(Button.B, on_button_pressed_b)
+_temperature = 25
+_acidVoltage = 2032.44
+_neutralVoltage = 1500
--- main.ts
+++ main.ts
@@ -1,33 +1,36 @@
-let _temperature = 25.0
-let _acidVoltage = 2032.44
-let _neutralVoltage = 1500.0
-let voltat7 = 0
-let vlotat4 = 0
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     basic.showString("Cal mode")
     basic.showString("Ph4")
-    vlotat4 = pins.analogReadPin(AnalogPin.P0) / 1024 * 5000
-})
-function on_button_pressed_b() {
-    
-    basic.showString("Cal mode")
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showNumber(1)
+    basic.pause(1000)
+    _acidVoltage = pins.analogReadPin(AnalogPin.P0) / 1024 * 5000
     basic.showString("Ph7")
-    voltat7 = pins.analogReadPin(AnalogPin.P0) / 1024 * 5000
-}
-
-input.onButtonPressed(Button.B, on_button_pressed_b)
-function read_PH(voltage: number, temperature: number): number {
-    
-    
-    let slope = (7.0 - 4.0) / ((_neutralVoltage - 1500.0) / 3.0 - (_acidVoltage - 1500.0) / 3.0)
-    let intercept = 7.0 - slope * (_neutralVoltage - 1500.0) / 3.0
-    let _phValue = slope * (voltage - 1500.0) / 3.0 + intercept
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showNumber(1)
+    basic.pause(1000)
+    _neutralVoltage = pins.analogReadPin(AnalogPin.P0) / 1024 * 5000
+})
+function read_PH (voltage: number, temperature: number) {
+    slope = (7 - 4) / ((_neutralVoltage - 1500) / 3 - (_acidVoltage - 1500) / 3)
+    intercept = 7 - slope * (_neutralVoltage - 1500) / 3
+    _phValue = slope * (voltage - 1500) / 3 + intercept
     return Math.round(_phValue)
 }
-
-function on_button_pressed_ab() {
-    read_PH(pins.analogReadPin(AnalogPin.P0) / 1024 * 5000, input.temperature())
-}
-
-input.onButtonPressed(Button.B, on_button_pressed_b)
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(read_PH(pins.analogReadPin(AnalogPin.P0) / 1024 * 5000, input.temperature()))
+})
+let _phValue = 0
+let intercept = 0
+let slope = 0
+let _neutralVoltage = 0
+let _acidVoltage = 0
+let _temperature = 25
+_acidVoltage = 2032.44
+_neutralVoltage = 1500
